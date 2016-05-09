0c518329dace91396af3c1ab178281f765bb7a46
Daniel Kelling <daniel.horus@gmail.com>
2016-05-08 22:59:16 -0300

    Quebrei em outra função. Ainda dá pra fazer mais duas ali pros comments.

--- libTextParser.py
+++ libTextParser.py
@@ -30,30 +30,14 @@ def parseText(g, text):
     d = [ [] for _ in t ]       # d is a list of dsets: d[0], d[1], ..., d[len(t)]
     d[0] = createD0(g)          # populate d[0]
 
-    # TODO: split this into more functions
-    # it's currently ugly af
+    # create d[n] for n = 1..len(t)
     for n, rsymbol in enumerate(t, start=1):    # loop through the words in the text counting them from 1
         for drule in d[n-1]:                    # look in the last dset
             dot = drule['dot']
-            if dot == drule['nsymbols']:        # this drule is already over, skip it
+            if dot == drule['nsymbols']:        # this drule is already finished, skip it
                 continue
             if drule['prod'][dot] == rsymbol:   # if this drule has the current word after the dot
-                newdrule = drule.copy()
-                newdrule['dot'] += 1
-                d[n].append(newdrule)           # copy it to the new dset, advancing the dot
-                if newdrule['dot'] == newdrule['nsymbols']:     # the dot reached the end of the drule
-                    slash = newdrule['slash']
-                    '''
-                    Expande do conjunto ['slash'] as regras que têm ['rname'] no índice ['dot'], avançando o ['dot']
-                    '''
-                    pass
-                else:
-                    nextsymbol = newdrule['prods'][ newdrule['dot'] ]
-                    if nextsymbol in g['rnames']:   # the dot reached a variable that needs expansion from g
-                        '''
-                        Expande ela da gramática com ['dot'] = 0 e ['slash'] = n
-                        '''
-                        pass
+                d = advanceDot(g, drule, d, n)  # advance the dot and propagate this all around dn
 
     # to accept the text, we must have a drule with
     for drule in d[n-1]:
@@ -91,3 +75,27 @@ def createD0(g):
                 rulestoproc.append(prod[0])             # list it to process
 
     return d0
+
+
+def advanceDot(g, drule, d, n):
+    """A word was processed in the drule; update dn according to the Earley algorithm"""
+
+    newdrule = drule.copy()
+    newdrule['dot'] += 1
+    d[n].append(newdrule)   # copy it to the new dset, advancing the dot
+
+    if newdrule['dot'] == newdrule['nsymbols']:     # the dot reached the end of the drule
+        slash = newdrule['slash']
+        '''
+        Expande do conjunto ['slash'] as regras que têm ['rname'] no índice ['dot'], avançando o ['dot']
+        '''
+        pass
+    else:
+        nextsymbol = newdrule['prods'][ newdrule['dot'] ]
+        if nextsymbol in g['rnames']:   # the dot reached a variable that needs expansion from g
+            '''
+            Expande ela da gramática com ['dot'] = 0 e ['slash'] = n
+            '''
+            pass
+
+    return d
