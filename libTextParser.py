c0e340933b7d4f82ce11c064e9238f0ba5394faf
Daniel Kelling <daniel.horus@gmail.com>
2016-05-09 01:14:35 -0300

    Parser funcionando, testem. Faltam árvores de derivação.

--- libTextParser.py
+++ libTextParser.py
@@ -27,21 +27,22 @@ def parseText(g, text):
     t = text.split()
     t[:] = map(str.strip, t)    # strip spaces from each word in the text
 
-    d = [ [] for _ in t ]       # d is a list of dsets: d[0], d[1], ..., d[len(t)]
-    d[0] = createD0(g)          # populate d[0]
+    d = [ [] for _ in t ]                               # d is a list of dsets: d[0], d[1], ..., d[len(t) - 1]
+    d.append( [] )                                      # we need a d[len(t)] as well
+    d[0] = expandFromGrammar(g, d[0], 0, g['start'])    # populate d[0] expanding the starting rule from the grammar
 
     # create d[n] for n = 1..len(t)
     for n, rsymbol in enumerate(t, start=1):    # loop through the words in the text counting them from 1
-        for drule in d[n-1]:                    # look in the last dset
+        for drule in d[n-1]:                    # look in the previous dset
             dot = drule['dot']
             if dot == drule['nsymbols']:        # this drule is already finished, skip it
                 continue
-            if drule['prod'][dot] == rsymbol:   # if this drule has the current word after the dot
-                d = advanceDot(g, drule, d, n)  # advance the dot and propagate this all around dn
+            if drule['prod'][dot] == rsymbol:       # if this drule has the current word after the dot
+                d[n] = advanceDot(g, drule, d, n)   # advance the dot and propagate this all around dn
 
-    # to accept the text, we must have a drule with
-    for drule in d[n-1]:
-        if(drule['rname'] == g['start'] and         # rname = starting symbol,
+    # to accept the text, we must have a drule in the last dset with:
+    for drule in d[n]:
+        if(drule['rname'] == g['start'] and         # rname as starting symbol,
            drule['dot'] == drule['nsymbols'] and    # the dot in the end of the production,
            drule['slash'] == 0):                    # and it must end with /0
             return True
@@ -49,12 +50,9 @@ def parseText(g, text):
     return False
 
 
-def createD0(g):
-    """Create the first set of drules: D0"""
-
-    d0 = []
-    rulestoproc = [g['start']]  # vars to process, starting on the starting symbol
-    rulesprocced = []           # vars already processed
+def expandFromGrammar(g, dset, slash, s):
+    rulestoproc = [s]       # vars to process
+    rulesprocced = []       # vars already processed
 
     while rulestoproc != []:        # while there are rules to process
         rule = rulestoproc.pop(0)   # take the current rule
@@ -65,37 +63,45 @@ def createD0(g):
                 'prod': prod,               # current production,
                 'nsymbols': len(prod),      # number of symbols in the production,
                 'dot': 0,                   # index of the string in the production that comes after the •,
-                'slash': 0,                 # and the immutable value of "/n"
+                'slash': slash,             # and the immutable value of "/n"
             }
-            d0.append(drule)
-            dotsymbbol = prod[ drule['dot'] ]           # now take the symbol right after the dot
-            if(dotsymbbol in g['rnames'] and            # if it's a variable
-               not dotsymbbol in rulesprocced and       # not yet processed
-               not dotsymbbol in rulestoproc):          # and not already listed to process
-                rulestoproc.append(prod[0])             # list it to process
+            dset.append(drule)
+            dotsymbbol = prod[ drule['dot'] ]       # now take the symbol right after the dot
+            if(dotsymbbol in g['rnames'] and        # if it's a variable
+               not dotsymbbol in rulesprocced and   # not yet processed
+               not dotsymbbol in rulestoproc):      # and not already listed to process
+                rulestoproc.append(prod[0])         # list it to process
 
-    return d0
+    return dset
 
 
 def advanceDot(g, drule, d, n):
-    """A word was processed in the drule; update dn according to the Earley algorithm"""
+    """Copy a drule to the current dset advancing the dot and propagating to other drules"""
 
-    newdrule = drule.copy()
-    newdrule['dot'] += 1
-    d[n].append(newdrule)   # copy it to the new dset, advancing the dot
+    newdrule = drule.copy() # do it this way, otherwise you're copying a pointer
+    newdrule['dot'] += 1    # advance the dot
+    d[n].append(newdrule)   # copy to the current dset
 
-    if newdrule['dot'] == newdrule['nsymbols']:     # the dot reached the end of the drule
+    if newdrule['dot'] == newdrule['nsymbols']:         # the dot reached the end of the drule
         slash = newdrule['slash']
-        '''
-        Expande do conjunto ['slash'] as regras que têm ['rname'] no índice ['dot'], avançando o ['dot']
-        '''
-        pass
+        rname = newdrule['rname']
+        d[n] = expandFromSlash(g, d, n, slash, rname)   # expand it from the slash dset
     else:
-        nextsymbol = newdrule['prods'][ newdrule['dot'] ]
-        if nextsymbol in g['rnames']:   # the dot reached a variable that needs expansion from g
-            '''
-            Expande ela da gramática com ['dot'] = 0 e ['slash'] = n
-            '''
-            pass
+        nextsymbol = newdrule['prod'][ newdrule['dot'] ]
+        if nextsymbol in g['rnames']:                           # the dot reached a variable
+            d[n] = expandFromGrammar(g, d[n], n, nextsymbol)    # expand it from the grammar
 
-    return d
+    return d[n]
+
+
+def expandFromSlash(g, d, n, slash, rname):
+    """Copy to dn all the drules in d[slash] that had rname after the dot, advancing the dot"""
+
+    for drule in d[slash]:                  # look in the slash dset
+        dot = drule['dot']
+        if dot == drule['nsymbols']:        # this drule is already finished, skip it
+            continue
+        if drule['prod'][dot] == rname:     # if this drule has the finished var after the dot
+            advanceDot(g, drule, d, n)      # advance the dot and propagate this all around dn
+
+    return d[n]
